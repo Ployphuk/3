6319d6db19e6a38b136e61cc75a77368c7d01b6d
Ployphuk <111555274+Ployphuk@users.noreply.github.com>
2023-03-28 22:48:01 +0700

    Add files via upload

---
+++ homework_3/1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+		
+
+        Console.Write("Enter P1 x point: ");
+		double P1x = double.Parse(Console.ReadLine());
+		Console.Write("Enter P1 y point: ");
+		double P1y = double.Parse(Console.ReadLine());
+		Console.Write("Enter P2 x Point: ");
+		double P2x = double.Parse(Console.ReadLine());
+		Console.Write("Enter P2 y point: ");
+		double P2y = double.Parse(Console.ReadLine());
+		Console.Write("Enter P3 x point: ");
+		double P3x = double.Parse(Console.ReadLine());
+		Console.Write("Enter P3 y point: ");
+		double P3y = double.Parse(Console.ReadLine());
+
+		CenterPoint(P1x, P1y, P2x,  P2y, P3x, P3y);
+	
+	
+    }
+
+	public static void CenterPoint(double P1x, double P1y, double P2x, double P2y, double P3x, double P3y){
+		
+		double h,k;
+		h = ((P2y - P1y)*(P3y*P3y-P1y*P1y+P3x*P3x-P1x*P1x)+(P3y-P1y)*(P1y*P1y-P2y*P2y+P1x*P1x-P2x*P2x))/(2*(P3x-P1x)*(P2y-P1y)-2*(P2x-P1x)*(P3y-P1y));
+		k = ((P2x-P1x)*(P3x*P3x-P1x*P1x+P3y*P3y-P1y*P1y)+(P3x-P1x)*(P1x*P1x-P2x*P2x+P1y*P1y-P2y*P2y))/(2*(P3y-P1y)*(P2x-P1x)-2*(P2y-P1y)*(P3x-P1x));
+
+		Console.WriteLine("Center point is {0}, {1}",h,k);
+		
+	}
+
+	public static  void Radius(double P1x, double P1y, double P2x, double P2y, double P3x, double P3y, double h, double k){
+		double r = Math.Sqrt((P1x - h)*(P1x - h)+(P1y - k)* (P1y - k));
+		Console.WriteLine("Radisu is {0}",r);
+	}
+
+	
+}
---
+++ homework_3/3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program{
+    static void Main(string[] args){
+        Console.WriteLine("Circle 1");
+        Console.Write("Enter Circle 1 center x:");
+        double c1x = double.Parse(Console.ReadLine());
+        Console.Write("Enter Circle 1 center y:");
+        double c1y = double.Parse(Console.ReadLine());
+        Console.Write("Enter Radius of Circle 1:");
+        double c1r = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Circle 2");
+        Console.Write("Enter Circle 2 center x:");
+        double c2x = double.Parse(Console.ReadLine());
+        Console.Write("Enter Circle 2 center y:");
+        double c2y = double.Parse(Console.ReadLine());
+        Console.Write("Enter Radius of Circle 2:");
+        double c2r = double.Parse(Console.ReadLine());
+
+        Distance(c1x, c1y, c2x, c2y);
+    }
+
+    static void Distance(double c1x, double c1y, double c2x, double c2y){
+        double result = Math.Sqrt(Math.Pow(c2x - c1x,2)+Math.Pow(c2y - c1y,2));
+
+        Console.WriteLine("Result is {0}", result);
+    }
+
+    static void CheckIntersect(double c1x, double c1y, double c1r, double c2x, double c2y, double c2r, double result){
+        if(result <= c1r + c2r){
+            double a = (Math.Pow(c1r,c2r)-Math.Pow(c2r,2)+Math.Pow(result,2))/(2*result);
+            double h = Math.Sqrt(Math.Pow(c1r,2) - Math.Pow(a, 2));
+
+            double centerx = c1x + a * (c2x - c1x)/result;
+            double centery = c1y + a* (c2y - c1y)/result;
+
+            double x = h*(c2y - c1y)/result;
+            double y = h*(c2x - c1x)/result;
+
+            if(result == c1r + c2r){
+                Console.WriteLine(centerx - x);
+                Console.WriteLine(centery - y);
+            }else{
+                Console.WriteLine(centerx - x);
+                Console.WriteLine(centery + y);
+                Console.WriteLine(centerx + x);
+                Console.WriteLine(centery - y);
+            }
+        }
+    }
+}
